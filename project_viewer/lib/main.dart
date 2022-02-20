498230e9ce67af2b52e1838e21846fcaab2909e6
Hendrik Ziegler <hendrikziegler@outlook.de>
2022-02-20 17:40:51 +0100

    added the url_strategy package to remove the annoying # infront of the flutter route

--- project_viewer/lib/main.dart
+++ project_viewer/lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:project_viewer/app_router.gr.dart';
+import 'package:url_strategy/url_strategy.dart';
 
 void main() {
+  setPathUrlStrategy();
   runApp(MyApp());
 }
 
